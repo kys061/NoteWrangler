c01c5734509025f9075c6ce2b9471c1fd8917ce5
kang <kys061@gmail.com>
2015-08-16 21:53:12 +0900

    first commit for js files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+angular.module("NoteWrangler",['ngRoute'])
+
+.config(function($routeProvider) {
+  $routeProvider.when('/notes', {
+    templateUrl: 'templates/pages/notes/index.html'
+  });
+
+});
